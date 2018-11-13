bd0557cc14b7d009ab9c0ea10513f9e4841f120c
Parth Patel <pvpprod89@gmail.com>
2018-11-12 20:40:45 -0500

    initial files

---
+++ routes/api/exampleApiRoute.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+
+// models example code
+// const resourceRoutes = require('./resource');
+
+// Model Resource routes example code
+// router.use('/resource', resourceRoutes);
+
+module.exports = router;
---
+++ routes/api/index.js
@@ -0,0 +1,3 @@
+module.exports = {
+  Resource: require('./resource') // ModelName: model location being exported for shorthand referencing
+};
