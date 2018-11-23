5c890f245440aee9705fba142e7780c22f3b8d4c
Parth Patel <pvpprod89@gmail.com>
2018-11-22 20:14:58 -0500

    deleted routes/api and not using mongoose for now

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,3 +1,3 @@
-module.exports = {
-  Resource: require('./resource') // ModelName: model location being exported for shorthand referencing
-};
+// module.exports = {
+//   Resource: require('./resource') // ModelName: model location being exported for shorthand referencing
+// };
